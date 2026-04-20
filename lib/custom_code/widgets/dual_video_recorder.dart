258cb9ed72ff8208787b40a8edf68871b2b49491
Mus'ab Hira <musabthonippadam@gmail.com>
2026-04-19 22:10:26 -0700

    Migrate to ffmpeg_kit_flutter_new_video to fix pod install 404

--- lib/custom_code/widgets/dual_video_recorder.dart
+++ lib/custom_code/widgets/dual_video_recorder.dart
@@ -1,7 +1,7 @@
 import 'dart:ui' as ui;
 import 'package:camerawesome/camerawesome_plugin.dart';
-import 'package:ffmpeg_kit_flutter_video/ffmpeg_kit.dart';
-import 'package:ffmpeg_kit_flutter_video/return_code.dart';
+import 'package:ffmpeg_kit_flutter_new_video/ffmpeg_kit.dart';
+import 'package:ffmpeg_kit_flutter_new_video/return_code.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:path_provider/path_provider.dart';
